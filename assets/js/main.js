8cc2c1563a4f7b124b2b38b531d309e97b8827c6
Jatin Nijhawan <jatin.kumar@daffodilsw.com>
2023-02-02 13:13:59 +0530

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,7 +112,7 @@ let resumeButton = document.getElementById('resume-button');
 /* Html2pdf options */
 let opt = {
   margin: 0,
-  filename: 'Jatin_Resume-Resume.pdf',
+  filename: 'Jatin_Resume.pdf',
   image: { type: 'jpeg', quality: 0.98 },
   html2canvas: { scale: 4 },
   jsPDF: { format: 'a4', orientation: 'portrait' },
